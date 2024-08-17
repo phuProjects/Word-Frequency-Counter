907141fd24805b49d672861b1e9b6191f4d3ff43
Phu Le <phu3262006@gmail.com>
2024-08-17 16:50:16 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,10 @@
 import re
 
 
-def word_freq_counter(file_name):
-
+def word_freq_counter(file_name, the_word):
 
     with open(file_name) as file:
         text = file.read()
-
 
     #Make all words lowercase
     text = text.lower()
@@ -14,9 +12,21 @@ def word_freq_counter(file_name):
     #removes all punctation with the pattern [^\w\s]. Make sure the r is before the pattern, indicating that it is a raw string.
     text = re.sub(r'[^\w\s]', '', text)
 
-    #create dict to store frequency of words. 
-    word = {}
+    #split the text into words
+    words = text.split()
 
+    #create dict to store frequency of words. 
+    word_count = {the_word: 0}
+
+    for word in words:
+        if word == the_word:
+            word_count[the_word] += 1
+
+    if word_count[the_word] >= 1:
+        print(f"{the_word}: {word_count[the_word]}")
+    else:
+        print("The word you are looking here is NOT in here.")
+    print("")
 
     return text
-print(word_freq_counter('test.txt'))
+print(word_freq_counter('test.txt', "the"))
